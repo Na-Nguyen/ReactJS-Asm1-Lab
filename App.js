f15a6d1aed01be3e16593a032aaebdf576586b59
Na Nguyen <nantnfx11407@funix.edu.vn>
2021-08-26 10:46:06 +0700

    Component part2

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './component/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    }
+  }
   render() {
     return (
       <div>
@@ -13,9 +20,8 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
-
     );
   }
 }
